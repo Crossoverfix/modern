513f7e7f1cd25cfec9255c4df90ec0d988823eec
Crossoverfix <39341735+Crossoverfix@users.noreply.github.com>
2019-06-20 19:07:26 +0300

    v1.5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,43 @@
 $(document).ready(function () {
-
+    var $switchSales = $('.top-sales__control');
+    $switchSales.on('click', function () {
+        $switchSales.removeClass('active');
+        $(this).addClass('active');
+        $('.top-sales__left__bg').css("background","url('" + $(this).attr('data-pic-left') + "')");
+        $('.top-sales__left__title').html($(this).attr('data-name'));
+        $('.top-sales__left__info a').html($(this).attr('data-price'));
+        $('.top-sales__right__bg').css("background","url('" + $(this).attr('data-pic-right') + "')");
+        return false;
+    })
+    var $switchSlider = $('.catalog__control');
+    $switchSlider.on('click', function () {
+        let $slider = $(this).attr('data-type');
+        if($slider == 'first'){
+            $('[data-slider-type]').removeClass('active');
+            $('[data-slider-type="first"]').addClass('active');
+            $('.catalog__switch__line__progress').css('width','8%');
+            $('.catalog__switch__line__point').css('left','8%');
+        } else  if($slider == 'second'){
+            $('[data-slider-type]').removeClass('active');
+            $('[data-slider-type="second"]').addClass('active');
+            $('.catalog__switch__line__progress').css('width','29%');
+            $('.catalog__switch__line__point').css('left','29%');
+        } else if($slider == 'third'){
+            $('[data-slider-type]').removeClass('active');
+            $('[data-slider-type="third"]').addClass('active');
+            $('.catalog__switch__line__progress').css('width','50%');
+            $('.catalog__switch__line__point').css('left','50%');
+        } else if($slider == 'four'){
+            $('[data-slider-type]').removeClass('active');
+            $('[data-slider-type="four"]').addClass('active');
+            $('.catalog__switch__line__progress').css('width','71%');
+            $('.catalog__switch__line__point').css('left','71%');
+        } else if($slider == 'five'){
+            $('[data-slider-type]').removeClass('active');
+            $('[data-slider-type="five"]').addClass('active');
+            $('.catalog__switch__line__progress').css('width','92%');
+            $('.catalog__switch__line__point').css('left','92%');
+        }
+        return false;
+    })
 })
